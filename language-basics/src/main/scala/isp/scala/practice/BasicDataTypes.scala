95fa497eff32b5daf077cd036d278265d541b338
Kamil Duda <kamil.duda2@ing.com>
2017-09-23 15:56:34 +0200

    Create solutions

--- language-basics/src/main/scala/isp/scala/practice/BasicDataTypes.scala
+++ language-basics/src/main/scala/isp/scala/practice/BasicDataTypes.scala
@@ -6,42 +6,42 @@ object BasicDataTypes {
     * @return true Boolean value
     */
   def defineBoolean(): Boolean = {
-    ???
+    true
   }
 
   /**
     * @return Byte value of -16
     */
   def defineByte(): Byte = {
-    ???
+    -16 toByte
   }
 
   /**
     * @return Int value of 42
     */
   def defineInteger(): Int = {
-    ???
+    42
   }
 
   /**
     * @return Long value of 1 500 250
     */
   def defineLong(): Long = {
-    ???
+    1500250L
   }
 
   /**
     * @return Float value of 1.5
     */
   def defineFloat(): Float = {
-    ???
+    1.5F
   }
 
   /**
     * @return Double value of 1.0
     */
   def defineDouble(): Double = {
-    ???
+    1.0
   }
 
 }
--- language-basics/src/main/scala/isp/scala/practice/ScalaMaps.scala
+++ language-basics/src/main/scala/isp/scala/practice/ScalaMaps.scala
@@ -8,17 +8,17 @@ object ScalaMaps {
   /**
     * @return a Map containing the given key and value
     */
-  def createMap(key: String, value: Int): Map[String, Int] = ???
+  def createMap(key: String, value: Int): Map[String, Int] = Map(key -> value)
 
   /**
     * @param dict a source Map
     * @param key  key to get a value for
     * @return a value from the "dict" that corresponds to the "key"
     */
-  def getValue(dict: Map[String, Int], key: String): Int = ???
+  def getValue(dict: Map[String, Int], key: String): Int = dict(key)
 
   /**
     * @return a collections of keys from the given map
     */
-  def getKeys(dict: Map[String, Int]): Seq[String] = ???
+  def getKeys(dict: Map[String, Int]): Seq[String] = dict.keys.toSeq
 }
